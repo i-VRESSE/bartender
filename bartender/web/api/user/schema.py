59bb7f14e366b4c6b5e9744eefe4ae2a3e1f6dcd
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-08-31 11:33:45 +0200

    In /api/users/profile return which social login (GitHub, Orcid, Google, etc.) are tied to user

--- bartender/web/api/user/schema.py
+++ bartender/web/api/user/schema.py
@@ -1,7 +1,20 @@
 from pydantic import BaseModel
 
 
+class OAuthAccountName(BaseModel):
+    """DTO for social provider name (OAuth account from GithHub or Orcid) of a user."""
+
+    oauth_name: str
+
+    class Config:
+        orm_mode = True
+
+
 class UserProfileInputDTO(BaseModel):
     """DTO for profile of current user model."""
 
     email: str
+    oauth_accounts: list[OAuthAccountName]
+
+    class Config:
+        orm_mode = True
