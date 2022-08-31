ebf24d7b5d6f03fffc35d65bcd2819bb2ecb17af
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-08-31 11:33:20 +0200

    Remove routes that require sending mail

--- bartender/web/users/router.py
+++ bartender/web/users/router.py
@@ -23,16 +23,11 @@ def include_users_routes(app: FastAPI) -> None:
         prefix="/auth",
         tags=["auth"],
     )
-    app.include_router(
-        fastapi_users.get_reset_password_router(),
-        prefix="/auth",
-        tags=["auth"],
-    )
-    app.include_router(
-        fastapi_users.get_verify_router(UserRead),
-        prefix="/auth",
-        tags=["auth"],
-    )
+    # Routes require sending mail, removed for now
+    # as we prefer social login over local account.
+    # See https://fastapi-users.github.io/fastapi-users/10.1/configuration/full-example/
+    # to add get_reset_password_router + get_verify_router back
+
     app.include_router(
         fastapi_users.get_users_router(UserRead, UserUpdate),
         prefix="/users",
@@ -61,3 +56,8 @@ def include_users_routes(app: FastAPI) -> None:
             prefix="/auth/associate/github",
             tags=["auth"],
         )
+        # TODO An access token given in the callback response.
+        # The Swagger UI does not allow to authorize with a access token.
+        # It would be nice to add JWT bearer security scheme to each protected route
+        # see https://spec.openapis.org/oas/v3.1.0#jwt-bearer-sample
+        # do this without breaking the username/password authorize in Swagger UI
