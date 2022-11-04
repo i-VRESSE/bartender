79d0a4c20ee3bf41f0c909ed4483d75fae149a3c
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2022-11-04 10:35:36 +0100

    Make type more narrow

--- bartender/filesystem/__init__.py
+++ bartender/filesystem/__init__.py
@@ -1,6 +1,7 @@
 """Filesystem module."""
 
 from pathlib import Path
+from typing import Literal
 
 from bartender.settings import settings
 
@@ -14,10 +15,10 @@ def setup_job_root_dir() -> None:
     job_root_dir.mkdir(exist_ok=True)
 
 
-def has_config_file(application: str, job_dir: Path) -> bool:
+def has_config_file(application: str, job_dir: Path) -> Literal[True]:
     """Check if config file required by application is present in job directory.
 
-    :param application: Application to check config file for.
+    :param application: Name of application to check config file for.
     :param job_dir: In which directory to look.
     :raises IndexError: When config file could not be found
     :return: True when found.
