c7170873fe5d6d15aad547e0f5470be5029ef417
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-11-17 16:26:47 +0100

    Use asyncssh to connect to Slurm container

--- bartender/schedulers/slurm.py
+++ bartender/schedulers/slurm.py
@@ -2,84 +2,133 @@ from abc import ABC, abstractmethod
 from asyncio import create_subprocess_exec
 from asyncio.subprocess import PIPE
 from textwrap import dedent
-from typing import IO, Any, Optional, Union
+from typing import Optional
+
+from asyncssh import connect
 
 from bartender.db.models.job_model import State
 from bartender.schedulers.abstract import AbstractScheduler, JobDescription
 
 
-class Terminal(ABC):
+class CommandRunner(ABC):
     @abstractmethod
-    async def exec(
+    async def run(
         command: str,
         *args: list[str],
-        stdout: Union[None, PIPE, str],
-        stderr: Union[None, PIPE, str] = None,
-        cwd: str = None,
-    ) -> tuple(str, str, str):
-        """Execute command."""
+        stdin: Optional[str] = None,
+        cwd: Optional[str] = None,
+    ) -> tuple[int, str, str]:
+        """Run command.
+
+        :param command: Command to execute. Command can not contain spaces.
+        :param args: List of arguments for command. Argument containing spaces should be wrapped in quotes.
+        :param stdin: Input for command.
+        :param cwd: In which directory the command should be run.
+        :return: Tuple with return code, stdout and stderr.
+        """
 
 
-class LocalTerminal(Terminal):
-    async def exec(
+class LocalCommandRunner(CommandRunner):
+    async def run(
+        self,
         command: str,
         *args: list[str],
-        stdin: Union[None, int, IO[Any]] = None,
-        stdout: Union[None, int, IO[Any]] = None,
-        stderr: Union[None, int, IO[Any]] = None,
-        cwd: str = None,
-    ) -> tuple(str, str, str):
+        stdin: Optional[str] = None,
+        cwd: Optional[str] = None,
+    ) -> tuple[int, str, str]:
         proc = await create_subprocess_exec(
-            command, *args, stdout=stdout, stderr=stderr, cwd=cwd
+            command, *args, stdout=PIPE, stderr=PIPE, cwd=cwd
         )
-        if stdout is None and stderr is None:
-            returncode = await proc.wait()
-            return (returncode, "", "")
-        else:
-            (stdout, stderr) = await proc.communicate(stdin)
-            return (proc.returncode, stdout, stderr)
+        (stdout, stderr) = await proc.communicate(stdin)
+        return (proc.returncode, stdout, stderr)
 
 
-class SSHTerminal(Terminal):
-    async def exec(
+class SSHCommandRunner(CommandRunner):
+    def __init__(
+        self,
+        hostname: str,
+        port: Optional[int] = None,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+    ):
+        self.hostname = hostname
+        self.port = port
+        self.username = username
+        self.password = password
+        self.conn = None
+
+    async def _connect(self):
+        conn_vargs = {
+            "known_hosts": None,
+        }
+        if self.password is not None:
+            conn_vargs["agent_path"] = None
+
+        self.conn = await connect(
+            host=self.hostname,
+            port=self.port,
+            username=self.username,
+            password=self.password,
+            **conn_vargs,
+        )
+
+    async def run(
+        self,
         command: str,
         *args: list[str],
-        stdin: Union[None, int, IO[Any]] = None,
-        stdout: Union[None, int, IO[Any]] = None,
-        stderr: Union[None, int, IO[Any]] = None,
-        cwd: str = None,
-    ) -> tuple(str, str, str):
-        # TODO find ssh client
-        raise NotImplementedError()
+        stdin: Optional[str] = None,
+        cwd: Optional[str] = None,
+    ) -> tuple[int, str, str]:
+        remote_command = command
+        if args:
+            remote_command = f'{remote_command} {" ".join(args)}'
+        if cwd is not None:
+            remote_command = f"cd {cwd} && {remote_command}"
+
+        if self.conn is None:
+            await self._connect()
+
+        result = await self.conn.run(remote_command, input=stdin)
+
+        print((command, args, result.returncode, result.stdout, result.stderr))
+        return (result.returncode, result.stdout, result.stderr)
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
 
 
 class SlurmScheduler(AbstractScheduler):
     def __init__(
         self,
-        terminal: Terminal,
+        runner: CommandRunner,
         partition: Optional[str] = None,
         time: Optional[str] = None,
         extra_options: list[str] = [],
     ):
         # TODO which option should be set to per scheduler or per job description?
-        self.terminal = terminal
+        self.runner = runner
         self.partition = partition
         self.time = time
         self.extra_options = extra_options
 
     async def submit(self, description: JobDescription) -> str:  # noqa: D102):
+        # TODO check that if runniner is a SSHCommandRunner then description.jobdir must be on a shared filesystem
         script = self._submit_script(description)
+        print(script)
         command = "sbatch"
-        proc = await create_subprocess_exec(
+        # command = 'hostname'
+        (returncode, stdout, stderr) = await self.runner.run(
             command,
-            stdout=PIPE,
-            stderr=PIPE,
             cwd=description.job_dir,
+            stdin=script,
         )
-        (stdout, stderr) = await proc.communicate(script)
-        if proc.returncode != 0:
+        if returncode != 0:
             raise RuntimeError(
-                f"Error running sbatch, exited with {proc.returncode}: {stderr}",
+                f"Error running sbatch, exited with {returncode}: {stderr}",
             )
 
         job_id = stdout.strip().split(" ")[-1]
@@ -88,17 +137,19 @@ class SlurmScheduler(AbstractScheduler):
     async def state(self, job_id: str) -> State:
         command = "squeue"
         args = ["-j", job_id, "--noheader", "--format=%T"]
-        proc = await create_subprocess_exec(
-            command,
-            *args,
-            stdout=PIPE,
-            stderr=PIPE,
-        )
-        (stdout, stderr) = await proc.communicate()
-        if proc.returncode != 0:
-            raise RuntimeError(
-                f"Error running squeue, exited with {proc.returncode}: {stderr}",
-            )
+        (returncode, stdout, stderr) = await self.runner.run(command, *args)
+        if returncode != 0 or stdout == "":
+            print("squeue failed")
+            # Completed jobs cannot be retrieved with squeue,
+            # depending on whether slurm has configured accounting and job is not too old
+            # the state can be requested using `sacct`
+            command = "sacct"
+            args = ["-j", job_id, "--noheader", "--format=state"]
+            (returncode, stdout, stderr) = await self.runner.run(command, *args)
+            if returncode != 0:
+                raise RuntimeError(
+                    f"Error running sacct, exited with {returncode}: {stderr}",
+                )
         slurm_state = stdout.strip()
         # See https://slurm.schedmd.com/squeue.html#SECTION_JOB-STATE-CODES
         status_map: dict[str, State] = {
@@ -125,12 +176,11 @@ class SlurmScheduler(AbstractScheduler):
     async def cancel(self, job_id: str) -> None:
         command = "scancel"
         args = [job_id]
-        proc = await create_subprocess_exec(
-            command,
-            *args,
-        )
-        await proc.wait()
+        await self.runner.run(command, *args)
         # TODO check if succesfull
+
+    async def close(self):
+        self.runner.close()
 
     def _submit_script(self, description: JobDescription) -> str:
         partition_line = ""
@@ -141,17 +191,17 @@ class SlurmScheduler(AbstractScheduler):
             time_line = f"#SBATCH --time={self.time}"
 
         # TODO filter out options already set
-        extra_option_lines = [f"#SBATCH {extra}\n" for extra in self.extra_options]
-        script = dedent(
-            f"""#!/bin/bash
-        {extra_option_lines}
-        {partition_line}
-        {time_line}
-        #SBATCH -output=stdout.txt
-        #SBATCH -error=stderr.txt
-        cd {description.job_dir}
-        {description.command}
-        echo -n $? > returncode
-        """,
+        extra_option_lines = "\n".join(
+            [f"#SBATCH {extra}" for extra in self.extra_options],
         )
-        return script
+        script = f"""\
+            #!/bin/bash
+            {extra_option_lines}
+            {partition_line}
+            {time_line}
+            #SBATCH --output=stdout.txt
+            #SBATCH --error=stderr.txt
+            {description.command}
+            echo -n $? > returncode
+        """
+        return dedent(script)
--- bartender/tests/schedulers/test_slurm.py
+++ bartender/tests/schedulers/test_slurm.py
@@ -1,11 +1,16 @@
+import asyncio
 from asyncio import sleep
 from pathlib import Path
 
+import asyncssh
 import pytest
+from asyncssh.misc import ConnectionLost
 from testcontainers.core.container import DockerContainer
+from testcontainers.core.waiting_utils import wait_container_is_ready
 
+from bartender.db.models.job_model import CompletedStates
 from bartender.schedulers.abstract import JobDescription
-from bartender.schedulers.slurm import SlurmScheduler, SSHTerminal
+from bartender.schedulers.slurm import SlurmScheduler, SSHCommandRunner
 
 
 class SlurmContainer(DockerContainer):
@@ -14,34 +19,70 @@ class SlurmContainer(DockerContainer):
         self.port_to_expose = 22
         self.with_exposed_ports(self.port_to_expose)
 
-    def terminal(self):
+    async def _ping(self):
+        vargs = {
+            "host": self.get_container_host_ip(),
+            "port": int(self.get_exposed_port(self.port_to_expose)),
+            "username": "xenon",
+            "password": "javagat",
+            "known_hosts": None,
+            "agent_path": None,
+        }
+        async with asyncssh.connect(**vargs) as conn:
+            await conn.run('echo "ping"', check=True)
+
+    @wait_container_is_ready(ConnectionLost)
+    def _connect(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self._ping())
+
+    def get_client(self):
         username = "xenon"
         password = "javagat"
         hostname = self.get_container_host_ip()
-        port = self.get_exposed_port(self.port_to_expose)
-        # TODO configure terminal
-        return SSHTerminal()
+        port = int(self.get_exposed_port(self.port_to_expose))
+        print((hostname, port))
+        return SSHCommandRunner(hostname, port, username, password)
+
+    def start(self):
+        super().start()
+        self._connect()
+        return self
 
 
 @pytest.fixture
-def slurm_server_terminal():
+def slurm_server():
     with SlurmContainer() as container:
-        yield container.terminal()
+        yield container
 
 
 @pytest.mark.anyio
-async def test_ok_running_job(tmp_path: Path, slurm_server_terminal) -> None:
+async def test_ok_running_job(tmp_path: Path, slurm_server) -> None:
     try:
-        scheduler = SlurmScheduler(slurm_server_terminal)
-        description = JobDescription(command="echo -n hello", job_dir=str(tmp_path))
+        client = slurm_server.get_client()
+        scheduler = SlurmScheduler(runner=client)
+        print("submit")
+        description = JobDescription(
+            command="echo -n hello"
+            # , job_dir=str(tmp_path)
+            # tmp_path is not owned by xenon user, so cd fails, use writeable path
+            ,
+            job_dir="/tmp",
+        )
 
         jid = await scheduler.submit(description)
 
-        # Wait for job to complete
-        # TODO poll state as slurm is slower
-        await sleep(0.01)
-        assert (await scheduler.state(jid)) == "ok"
-        assert (tmp_path / "returncode").read_text() == "0"
-        assert (tmp_path / "stdout.txt").read_text() == "hello"
+        print("submitted")
+        print(jid)
+        for _ in range(30):
+            state = await scheduler.state(jid)
+            if state in CompletedStates:
+                break
+            await sleep(0.5)
+
+        assert state == "ok"
+        # TODO deal with files in container
+        # assert (tmp_path / "returncode").read_text() == "0"
+        # assert (tmp_path / "stdout.txt").read_text() == "hello"
     finally:
         await scheduler.close()
