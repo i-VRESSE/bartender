1e5c2ce3d788b8bc46fbe1ac9ebd33d7bfe138fb
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-11-18 13:55:13 +0100

    Add ssh/sftp support to test against slurm docker container.

--- bartender/schedulers/slurm.py
+++ bartender/schedulers/slurm.py
@@ -1,128 +1,69 @@
-from abc import ABC, abstractmethod
-from asyncio import create_subprocess_exec
-from asyncio.subprocess import PIPE
 from textwrap import dedent
 from typing import Optional
 
-from asyncssh import connect
-
 from bartender.db.models.job_model import State
 from bartender.schedulers.abstract import AbstractScheduler, JobDescription
+from bartender.schedulers.runner import CommandRunner
 
 
-class CommandRunner(ABC):
-    @abstractmethod
-    async def run(
-        command: str,
-        *args: list[str],
-        stdin: Optional[str] = None,
-        cwd: Optional[str] = None,
-    ) -> tuple[int, str, str]:
-        """Run command.
-
-        :param command: Command to execute. Command can not contain spaces.
-        :param args: List of arguments for command. Argument containing spaces should be wrapped in quotes.
-        :param stdin: Input for command.
-        :param cwd: In which directory the command should be run.
-        :return: Tuple with return code, stdout and stderr.
-        """
-
-
-class LocalCommandRunner(CommandRunner):
-    async def run(
-        self,
-        command: str,
-        *args: list[str],
-        stdin: Optional[str] = None,
-        cwd: Optional[str] = None,
-    ) -> tuple[int, str, str]:
-        proc = await create_subprocess_exec(
-            command, *args, stdout=PIPE, stderr=PIPE, cwd=cwd
-        )
-        (stdout, stderr) = await proc.communicate(stdin)
-        return (proc.returncode, stdout, stderr)
-
-
-class SSHCommandRunner(CommandRunner):
-    def __init__(
-        self,
-        hostname: str,
-        port: Optional[int] = None,
-        username: Optional[str] = None,
-        password: Optional[str] = None,
-    ):
-        self.hostname = hostname
-        self.port = port
-        self.username = username
-        self.password = password
-        self.conn = None
-
-    async def _connect(self):
-        conn_vargs = {
-            "known_hosts": None,
-        }
-        if self.password is not None:
-            conn_vargs["agent_path"] = None
-
-        self.conn = await connect(
-            host=self.hostname,
-            port=self.port,
-            username=self.username,
-            password=self.password,
-            **conn_vargs,
-        )
-
-    async def run(
-        self,
-        command: str,
-        *args: list[str],
-        stdin: Optional[str] = None,
-        cwd: Optional[str] = None,
-    ) -> tuple[int, str, str]:
-        remote_command = command
-        if args:
-            remote_command = f'{remote_command} {" ".join(args)}'
-        if cwd is not None:
-            remote_command = f"cd {cwd} && {remote_command}"
-
-        if self.conn is None:
-            await self._connect()
-
-        result = await self.conn.run(remote_command, input=stdin)
-
-        print((command, args, result.returncode, result.stdout, result.stderr))
-        return (result.returncode, result.stdout, result.stderr)
-
-    def __del__(self):
-        self.close()
-
-    def close(self):
-        if self.conn:
-            self.conn.close()
+def _map_slurm_state(slurm_state: str) -> State:
+    status_map: dict[str, State] = {
+        "PENDING": "queued",
+        "CONFIGURING": "queued",
+        "RUNNING": "running",
+        "SUSPENDED": "running",
+        "COMPLETING": "running",
+        "STAGE_OUT": "running",
+        "CANCELLED": "error",
+        "COMPLETED": "ok",
+        "FAILED": "error",
+        "TIMEOUT": "error",
+        "PREEMPTED": "error",
+        "NODE_FAIL": "error",
+        "SPECIAL_EXIT": "error",
+    }
+    try:
+        return status_map[slurm_state]
+    except KeyError:
+        # fallback to error when slurm state code is unmapped.
+        return "error"
 
 
 class SlurmScheduler(AbstractScheduler):
+    """Slurm batch scheduler."""
+
     def __init__(
         self,
         runner: CommandRunner,
         partition: Optional[str] = None,
         time: Optional[str] = None,
-        extra_options: list[str] = [],
+        extra_options: Optional[list[str]] = None,
     ):
+        """Constructor.
+
+        :param runner: Runner for running commands. Can be local or ssh.
+        :param partition: Partition in which all jobs should be submitted.
+        :param time: Limit on the total run time of the job.
+        :param extra_options: Escape hatch to add extra options to job script.
+            The string `#SBATCH {extra_options[0]}` will be appended to job script.
+        """
         # TODO which option should be set to per scheduler or per job description?
         self.runner = runner
         self.partition = partition
         self.time = time
-        self.extra_options = extra_options
+        if extra_options is None:
+            self.extra_options = []
+        else:
+            self.extra_options = extra_options
 
     async def submit(self, description: JobDescription) -> str:  # noqa: D102):
-        # TODO check that if runniner is a SSHCommandRunner then description.jobdir must be on a shared filesystem
+        # TODO if runner is a SSHCommandRunner then description.jobdir
+        # must be on a shared filesystem or remote filesystem
         script = self._submit_script(description)
-        print(script)
         command = "sbatch"
-        # command = 'hostname'
         (returncode, stdout, stderr) = await self.runner.run(
             command,
+            args=[],
             cwd=description.job_dir,
             stdin=script,
         )
@@ -131,56 +72,52 @@ class SlurmScheduler(AbstractScheduler):
                 f"Error running sbatch, exited with {returncode}: {stderr}",
             )
 
-        job_id = stdout.strip().split(" ")[-1]
-        return job_id
+        return stdout.strip().split(" ")[-1]
 
     async def state(self, job_id: str) -> State:
-        command = "squeue"
+        """Get state of a job.
+
+        Once job is completed, then scheduler can forget job.
+
+        :param job_id: Identifier of job.
+        :return: State of job.
+        """
         args = ["-j", job_id, "--noheader", "--format=%T"]
-        (returncode, stdout, stderr) = await self.runner.run(command, *args)
+        (returncode, stdout, stderr) = await self.runner.run("squeue", args)
         if returncode != 0 or stdout == "":
-            print("squeue failed")
-            # Completed jobs cannot be retrieved with squeue,
-            # depending on whether slurm has configured accounting and job is not too old
+            # Completed jobs cannot be retrieved with squeue, depending on
+            # whether slurm has configured accounting and job is not too old
             # the state can be requested using `sacct`
-            command = "sacct"
-            args = ["-j", job_id, "--noheader", "--format=state"]
-            (returncode, stdout, stderr) = await self.runner.run(command, *args)
-            if returncode != 0:
-                raise RuntimeError(
-                    f"Error running sacct, exited with {returncode}: {stderr}",
-                )
+            stdout = await self._state_from_accounting(job_id)
         slurm_state = stdout.strip()
         # See https://slurm.schedmd.com/squeue.html#SECTION_JOB-STATE-CODES
-        status_map: dict[str, State] = {
-            "PENDING": "queued",
-            "CONFIGURING": "queud",
-            "RUNNING": "running",
-            "SUSPENDED": "running",
-            "COMPLETING": "running",
-            "STAGE_OUT": "running",
-            "CANCELLED": "error",
-            "COMPLETED": "ok",
-            "FAILED": "error",
-            "TIMEOUT": "error",
-            "PREEMPTED": "error",
-            "NODE_FAIL": "error",
-            "SPECIAL_EXIT": "error",
-        }
-        try:
-            return status_map[slurm_state]
-        except KeyError:
-            # fallback to error when slurm state code is unmapped.
-            return "error"
+        return _map_slurm_state(slurm_state)
 
     async def cancel(self, job_id: str) -> None:
+        """Cancel a queued or running job.
+
+        Once a queued job is cancelled, then the scheduler can forget job.
+
+        :param job_id: Identifier of job.
+        """
         command = "scancel"
         args = [job_id]
-        await self.runner.run(command, *args)
+        await self.runner.run(command, args)
         # TODO check if succesfull
 
-    async def close(self):
+    async def close(self) -> None:
+        """Cancel all runnning jobs and make scheduler unable to work."""
         self.runner.close()
+
+    async def _state_from_accounting(self, job_id: str) -> str:
+        command = "sacct"
+        args = ["-j", job_id, "--noheader", "--format=state"]
+        (returncode, stdout, stderr) = await self.runner.run(command, args)
+        if returncode != 0:
+            raise RuntimeError(
+                f"Error running sacct, exited with {returncode}: {stderr}",
+            )
+        return stdout
 
     def _submit_script(self, description: JobDescription) -> str:
         partition_line = ""
