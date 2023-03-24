981ab4de6e5b5c6c1272131cac47c5757145b4af
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-03-24 13:03:34 +0100

    We dont have static assets for sphinx so dont contigure
    Gets rid of warning during build.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,6 @@ myst_fence_as_directive = ["mermaid"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
 
 autoapi_type = "python"
 autoapi_dirs = ["../src"]
